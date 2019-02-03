62f289bb3cd1a6adefb6a6c898b94bbc36a61d5a
Shootacean <shootacean@gmail.com>
2019-02-03 13:48:00 +0900

    [clean] go fmt

--- beginners-selection/ABC081A_PlacingMarbles/main.go
+++ beginners-selection/ABC081A_PlacingMarbles/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 var sc = bufio.NewScanner(os.Stdin)
 
 func main() {
-    sc.Split(bufio.ScanWords)
-    s := readString()
-    answer := strings.Count(s, "1")
-    fmt.Printf("%v\n", answer)
+	sc.Split(bufio.ScanWords)
+	s := readString()
+	answer := strings.Count(s, "1")
+	fmt.Printf("%v\n", answer)
 }
 
 func readString() string {
-    sc.Scan()
-    return sc.Text()
+	sc.Scan()
+	return sc.Text()
 }
