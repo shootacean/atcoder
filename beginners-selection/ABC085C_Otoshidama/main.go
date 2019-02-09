12b08a1bdfa5c0d59e238f5db610bfe4e1daa298
Shootacean <shootacean@gmail.com>
2019-02-09 16:43:40 +0900

    [add] Solved beginners-selection ABC085C_Otoshidama - https://atcoder.jp/contests/abs/tasks - ２種類のお札の枚数がわかった時点で、もう１種類の枚数は計算できる

--- beginners-selection/ABC085C_Otoshidama/main.go
+++ beginners-selection/ABC085C_Otoshidama/main.go
@@ -11,10 +11,10 @@ var sc = bufio.NewScanner(os.Stdin)
 
 func main() {
 	sc.Split(bufio.ScanWords)
-
+	
 	N := readInt()
 	Y := readInt()
-
+	
 	x, y, z := solve(N, Y)
 	fmt.Printf("%d %d %d\n", x, y, z)
 }
@@ -34,28 +34,25 @@ func readInt() int {
 
 func solve(num, price int) (x, y, z int) {
 	cannotSolve := func() (int, int, int) { return -1, -1, -1 }
-
-	limit10k := price / 10000
-	limit5k := price / 5000
-	limit1k := price / 1000
-
+	
+	price = price / 1000
+	
+	limit10k := price / 10
+	limit5k := price / 5
+	
 	for i10k := limit10k; 0 <= i10k; i10k-- {
 		for i5k := limit5k; 0 <= i5k; i5k-- {
 			if num < (i10k + i5k) {
 				continue
 			}
-			for i1k := limit1k; 0 <= i1k; i1k-- {
-				n := i10k + i5k + i1k
-				if num < n {
-					continue
-				}
-				p := i10k*10000 + i5k*5000 + i1k*1000
-				if num == n && price == p {
-					x = i10k
-					y = i5k
-					z = i1k
-					return
-				}
+			i1k := num - i10k - i5k
+			n := i10k + i5k + i1k
+			p := i10k*10 + i5k*5 + i1k
+			if num == n && price == p {
+				x = i10k
+				y = i5k
+				z = i1k
+				return
 			}
 		}
 	}
