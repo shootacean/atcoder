d5a40abd6312bf9aceff86770ab0ef6c491b913b
Shootacean <shootacean@gmail.com>
2019-02-12 21:56:30 +0900

    [add] Solved ABC_001 A - https://atcoder.jp/contests/abc001/tasks

---
+++ ABC/ABC_001/A_積雪深差/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	h1 := readInt()
+	h2 := readInt()
+	fmt.Println(h1 - h2)
+}
+
+func readString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func readInt() int {
+	i, e := strconv.Atoi(readString())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
