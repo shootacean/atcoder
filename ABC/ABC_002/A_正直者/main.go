6cdca827fedacd1965c2cf387cc8726e63f260b3
Shota Narisaka <shootacean@gmail.com>
2019-02-13 22:31:31 +0900

    [add] Solved ABC_002 AB - https://atcoder.jp/contests/abc002/tasks

---
+++ ABC/ABC_002/A_正直者/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	x := readInt()
+	y := readInt()
+	var answer int
+	if x > y {
+		answer = x
+	} else {
+		answer = y
+	}
+	fmt.Println(answer)
+}
+
+func read() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func readInt() int {
+	i, e := strconv.Atoi(read())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
