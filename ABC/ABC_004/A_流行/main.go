d0a54f9c026c22fb7b157d199a8f86a5cc32611a
Shootacean <shootacean@gmail.com>
2019-02-18 21:47:02 +0900

    [add] Solved ABC_004 AB - https://atcoder.jp/contests/abc004/tasks

---
+++ ABC/ABC_004/A_流行/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	n := readInt()
+	fmt.Println(n * 2)
+}
+
+func read() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func readInt() int {
+	i, e := strconv.Atoi(read())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
