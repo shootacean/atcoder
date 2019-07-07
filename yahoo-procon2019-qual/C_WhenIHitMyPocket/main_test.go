25162426d9bc7adbeae59bf8c5cf3bbc53dcc44f
Shota Narisaka <shootacean@gmail.com>
2019-07-07 11:27:09 +0900

    [clean] go-fmt

--- yahoo-procon2019-qual/C_WhenIHitMyPocket/main_test.go
+++ yahoo-procon2019-qual/C_WhenIHitMyPocket/main_test.go
@@ -10,11 +10,11 @@ func TestSolve(t *testing.T) {
 	if act != 7 {
 		t.Fatalf("Failed test: %v: 4,2,6", act)
 	}
-	act = solve(7,3,4)
+	act = solve(7, 3, 4)
 	if act != 8 {
 		t.Fatalf("Failed test: %v: 7,3,4", act)
 	}
-	act = solve(314159265,35897932,384626433)
+	act = solve(314159265, 35897932, 384626433)
 	if act != 48518828981938099 {
 		t.Fatalf("Failed test: %v: 314159265,35897932,384626433", act)
 	}
