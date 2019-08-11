3ff9fc24a758bb7e47c36b3e6362b390f8744aa9
Shota Narisaka <shootacean@gmail.com>
2019-08-11 10:51:20 +0900

    [add] Solved ABC_115 A - https://atcoder.jp/contests/abc115/tasks

---
+++ ABC/ABC_115/A/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	const xmas = 25
+	d := readInt()
+	
+	fmt.Println("Christmas" + strings.Repeat(" Eve", xmas - d))
+}
+
+func read() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func readInt() int {
+	i, e := strconv.Atoi(read())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
