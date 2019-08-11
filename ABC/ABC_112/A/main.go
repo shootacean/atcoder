80ad2382b6e9269908872878c9beab7b0570d4f5
Shota Narisaka <shootacean@gmail.com>
2019-08-11 23:54:41 +0900

    [add] Solved ABC_112 A - https://atcoder.jp/contests/abc112/tasks

---
+++ ABC/ABC_112/A/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	x1 := readInt()
+	if x1 == 1 {
+		fmt.Println("Hello World")
+		return
+	}
+	a := readInt()
+	b := readInt()
+	fmt.Println(a + b)
+}
+
+func read() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func readInt() int {
+	i, e := strconv.Atoi(read())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
